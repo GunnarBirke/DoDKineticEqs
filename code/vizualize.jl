7dd8af70e6acaab1beb357052a56bbbdb51e37ad
louispetri <159759248+louispetri@users.noreply.github.com>
2024-11-10 14:42:38 +0100

    Includning legends/fixed axes

--- code/vizualize.jl
+++ code/vizualize.jl
@@ -1,10 +1,16 @@
 using Plots
-function vizualize(data, x_d, t_d; steplim = length(data[1,:]), u_exact = 0)  
+function vizualize(data, x_d, t_d; steplim = length(data[1,:]), u_exact = 0, clabel = 0) 
+    if clabel == 0
+        labelstring = ""
+        else
+        labelstring = clabel * ": "
+    end
+    ylimits = [minimum(minimum(data)), maximum(maximum(data))]
     anim = @animate for i ∈ 1:steplim
-        plot(legend = :outertopright, title = "T = $(t_d[i])")
-        plot!(x_d, data[:,i], label = "u_approx", yrange = [-2.5, 2])
+        plot(legend = :outertopright, title = "T = $(t_d[i])", ylims = ylimits)
+        plot!(x_d, data[:,i], label = labelstring * "approx")
         if u_exact != 0
-            plot!(x_d ,u_exact[:,i],label = "u_exact")
+            plot!(x_d ,u_exact[:,i],label = labelstring * "exact")
         end
     end
     return gif(anim, fps = 100)
